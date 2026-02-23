044ed965d7b9143f3201650cb03b792183ab268f
Warda-Samy <wardasamy1996@gmail.com>
2026-02-24 01:07:20 +0200

    Fix React imports and BrowserRouter, adjust footer issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
+import React from "react"; // لازم عشان JSX
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; // إضافة الـ BrowserRouter
+
 import App from "./App";
 import Navbar from "./navbar/NavBar";
 import Footer from "./footer/Footer";
 import "./App.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter basename="/heliore-chatbot">
+  <React.StrictMode>
+    <BrowserRouter basename="/pioneerse_schoo">
       <Navbar />
       <App />
       <Footer />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 );
